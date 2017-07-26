24d675a0fe17157228c6466e796d8e6bda966536
SinethembaDlova <dSinethemba@gmail.com>
2017-07-26 11:21:35 +0200

    adding js file

---
+++ sudoCode.js
@@ -0,0 +1,16 @@
+/*Most Smarties Pseudo Code:
+
+1.  Create a variable intSmarties = 0.
+2.  Create a list of colours representing the colours of the smarties in the box as a string separated by a comma.
+3.  Create a function called mostNumberSmarteis that takes in the color string as a parameter.
+5.  Split the parameter string by comma into a list of colours.
+6.  Create a variable called smartieMap = {}.
+7.  Loop over the list of colours.
+8.  Create a colours variable that saves each colour in the list.
+9.  If the smartieMap object contains the current colour add the value to the SmartieMap object and increment the value.
+10. If the smartieMap object doesn’t contain the current colour add the value to the smartieMap object but the value = 0.
+11. Create a variable called mostSmarties as a object mostSmarties = {}.
+12. Loop over the smartieMap.
+I3. f the smartieMap’s current colour is the highest save the new value with colour in the mostSmarties object.
+14. Return the mostSmarties object.
+*/
